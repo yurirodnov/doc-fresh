f9d23d10f9c2dd19d57615c9505cf313e120b93c
yuri_rodnov <somewaystodo@yandex.ru>
2026-09-05 23:41:12 +0300

    feat(UI): add max links allowed

--- front/src/components/hint/Hint.tsx
+++ front/src/components/hint/Hint.tsx
@@ -6,7 +6,8 @@ export const Hint = () => {
       <span>?</span>
       <div className={styles.hints}>
         <p>- Max file size allowed - 5 MB</p>
-        <p>- File format allowed - .txt, .pdf</p>
+        <p>- File format allowed - .txt, .pdf, .docx</p>
+        <p>- Max links count in file allowed - 120</p>
       </div>
     </div>
   );
